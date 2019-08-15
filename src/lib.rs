2ce09b27c6cb33bd0c7484e25bcba1923c3febce
Brendan Molloy <brendan@bbqsrc.net>
2019-08-15 17:36:34 +0200

    Add cmp for unwrapped type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 macro_rules! finite {
     ($name:tt, $ty:ty) => (
         finite!($name, $ty, stringify!($ty));
@@ -30,16 +28,28 @@ macro_rules! finite {
             }
         }
 
+        impl PartialEq<$ty> for $name {
+            fn eq(&self, other: &$ty) -> bool {
+                &self.0 == other
+            }
+        }
+
         impl Eq for $name {}
 
         impl PartialOrd for $name {
-            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-                Some(self.cmp(other))
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                self.0.partial_cmp(&other.0)
+            }
+        }
+
+        impl PartialOrd<$ty> for $name {
+            fn partial_cmp(&self, other: &$ty) -> Option<std::cmp::Ordering> {
+                self.0.partial_cmp(other)
             }
         }
 
         impl Ord for $name {
-            fn cmp(&self, other: &Self) -> Ordering {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
                 self.0.partial_cmp(&other.0).expect("must be finite")
             }
         }
@@ -64,5 +74,29 @@ mod tests {
         assert!(FiniteF64::new(f64::INFINITY).is_none());
         assert!(FiniteF32::new(f32::NEG_INFINITY).is_none());
         assert!(FiniteF64::new(f64::NEG_INFINITY).is_none());
+    }
+
+    #[test]
+    fn cmp32() {
+        let finite = FiniteF32::new(1f32).unwrap();
+        assert_eq!(finite < 32f32, true);
+        assert_eq!(finite == 1f32, true);
+        assert_eq!(finite > -1f32, true);
+
+        assert_eq!(finite > f32::NAN, false);
+        assert_eq!(finite > f32::NEG_INFINITY, true);
+        assert_eq!(finite < f32::INFINITY, true);
+    }
+
+    #[test]
+    fn cmp64() {
+        let finite = FiniteF64::new(1f64).unwrap();
+        assert_eq!(finite < 64f64, true);
+        assert_eq!(finite == 1f64, true);
+        assert_eq!(finite > -1f64, true);
+
+        assert_eq!(finite > f64::NAN, false);
+        assert_eq!(finite > f64::NEG_INFINITY, true);
+        assert_eq!(finite < f64::INFINITY, true);
     }
 }
